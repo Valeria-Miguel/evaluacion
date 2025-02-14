e12f454c122b851c87b77b4f4bfaa6f6799186aa
Valeria-Miguel <2022371092@uteq.edu.mx>
2025-02-13 23:22:24 -0600

    modi

--- users/permissions.py
+++ users/permissions.py
@@ -8,12 +8,12 @@ class IsAdminOrEmpleado(permissions.BasePermission):
 
     def has_permission(self, request, view):
         if not request.user or not request.user.is_authenticated:
-            return False  # Bloquea a usuarios no autenticados
+            return False  # bloquea a usuarios no autenticados
 
         if request.method in permissions.SAFE_METHODS:
-            return True  # Permite GET, HEAD, OPTIONS a todos los autenticados
+            return True  # permite GET, HEAD, OPTIONS a todos los autenticados
 
-        # Solo Admin y Empleado pueden hacer POST, PUT
+        # solo admin y empleado pueden hacer POST, PUT
         is_admin = request.user.groups.filter(name="Administrador").exists()
         is_empleado = request.user.groups.filter(name="Empleado").exists() and request.user.has_perm('users.change_customuser')
 
@@ -36,12 +36,12 @@ class BasePedidoPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         if not request.user or not request.user.is_authenticated:
-            return False  # Bloquea a usuarios no autenticados
+            return False  #bloquea a usuarios no autenticados
 
         if request.method in permissions.SAFE_METHODS:  # GET, HEAD, OPTIONS
-            return True  # Todos los autenticados pueden ver
+            return True  #todos los autenticados pueden ver
 
-        # Solo Admin y Empleado pueden hacer POST, PUT, DELETE
+        #solo admin y empleado pueden hacer POST, PUT, DELETE
         is_admin = request.user.groups.filter(name="Administrador").exists()
         is_empleado = request.user.groups.filter(name="Empleado").exists()
 
@@ -55,7 +55,7 @@ class BasePedidoPermission(permissions.BasePermission):
         is_empleado = request.user.groups.filter(name="Empleado").exists()
 
         if request.method == "DELETE":
-            # Admin puede eliminar cualquier pedido, Empleado solo los suyos
+            # admin puede eliminar cualquier pedido, empleado solo los suyos
             return is_admin or (is_empleado and obj.usuario == request.user)
 
-        return True  # Para otros métodos, usa las reglas generales
+        return True  # para otros metodos, usa las reglas generales
